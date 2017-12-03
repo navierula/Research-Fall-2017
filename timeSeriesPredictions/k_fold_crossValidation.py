0459a2f1169b53f041eb20afb2b983a2fba8153d
Navraj Narula <navrajnarula@gmail.com>
2017-12-03 17:51:42 -0500

    attempt k fold cross val

--- timeSeriesPredictions/k_fold_crossValidation.py
+++ timeSeriesPredictions/k_fold_crossValidation.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
 """
 @navrajnarula
+http://thelillysblog.com/2017/08/18/machine-learning-k-fold-validation/
 """
+import nltk # needed for Naive-Bayes
+import numpy as np
+from sklearn.model_selection import KFold
+
+import pandas
+from pandas import read_csv
+from pandas import datetime
+
+data = read_csv('data/130N_Cycles_1-47.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+
+# data is an array with our already pre-processed dataset examples
+kf = KFold(n_splits=3)
+sum = 0
+for train, test in kf.split(data):
+    train_data = np.array(data)[train]
+    test_data = np.array(data)[test]
+    classifier = nltk.NaiveBayesClassifier.train(train_data)
+    sum += nltk.classify.accuracy(classifier, test_data)
+average = sum/3
 
